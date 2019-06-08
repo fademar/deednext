982536b0d1489b9c447b95dc7a662ad05b582dc8
fademar <fdemarthon@gmail.com>
2019-06-08 21:14:44 +0200

    second commit

--- next.config.js
+++ next.config.js
@@ -1,32 +1,12 @@
 // ./next.config.js
 
 require("dotenv").config();
-const withCSS = require("@zeit/next-css");
+
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
 
-module.exports = withCss({
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      const antStyles = /antd\/.*?\/style\/css.*?/;
-      const origExternals = [...config.externals];
-      config.externals = [
-        (context, request, callback) => {
-          if (request.match(antStyles)) return callback();
-          if (typeof origExternals[0] === "function") {
-            origExternals[0](context, request, callback);
-          } else {
-            callback();
-          }
-        },
-        ...(typeof origExternals[0] === "function" ? [] : origExternals)
-      ];
-
-      config.module.rules.unshift({
-        test: antStyles,
-        use: "null-loader"
-      });
-    }
+module.exports = {
+  webpack: config => {
     config.plugins = config.plugins || [];
 
     config.plugins = [
@@ -38,6 +18,7 @@ module.exports = withCss({
         systemvars: true
       })
     ];
+
     return config;
   }
-});
+};
