a2a241e06d3ca205491ee2f416aa5ad6c0f9af49
fademar <fdemarthon@gmail.com>
2019-06-08 14:17:14 +0200

    broke

--- next.config.js
+++ next.config.js
@@ -1,12 +1,32 @@
 // ./next.config.js
 
 require("dotenv").config();
-
+const withCSS = require("@zeit/next-css");
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
 
-module.exports = {
-  webpack: config => {
+module.exports = withCss({
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style\/css.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === "function" ? [] : origExternals)
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: "null-loader"
+      });
+    }
     config.plugins = config.plugins || [];
 
     config.plugins = [
@@ -18,7 +38,6 @@ module.exports = {
         systemvars: true
       })
     ];
-
     return config;
   }
-};
+});
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,12 +33,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-            crossorigin="anonymous"
-          />
+          <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
           <Main />
